14957703175505ac005d63481fbb0a46bd61484d
binwon-Song <90763389+binwon-Song@users.noreply.github.com>
2023-07-02 23:30:42 +0900

    Time: 118 ms, Memory: 16.6 MB - LeetHub

--- swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -3,6 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head.next:
+#             return head
+#         head.val,head.next.val=head.next.val,head.val
+#         print(head)
+#         if head.next.next:
+#             self.swapPairs(head.next.next)
+        
+#         return head
+        
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Base case: 노드가 없거나 마지막 노드인 경우
@@ -13,8 +24,6 @@ class Solution:
         head.val, head.next.val = head.next.val, head.val
         
         # 재귀적으로 다음 두 노드 호출
-        head.next.next = self.swapPairs(head.next.next)
-        
+        self.swapPairs(head.next.next)
+        print(head)
         return head
-
-        
