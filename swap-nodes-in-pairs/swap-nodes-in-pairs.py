8be56a453812b7ade56acf05496009e1150c58f8
binwon-Song <90763389+binwon-Song@users.noreply.github.com>
2023-07-02 23:20:47 +0900

    Time: 36 ms, Memory: 16.4 MB - LeetHub

---
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Base case: 노드가 없거나 마지막 노드인 경우
+        if not head or not head.next:
+            return head
+        
+        # 두 노드의 값 교환
+        head.val, head.next.val = head.next.val, head.val
+        
+        # 재귀적으로 다음 두 노드 호출
+        head.next.next = self.swapPairs(head.next.next)
+        
+        return head
+
+        
